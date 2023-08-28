53f954c493896e6bf51d72b53a62e456e629c806
Bruno Facello <bfacello@gmail.com>
2023-08-27 21:58:15 -0300

    saludodiv

---
+++ eventos.js
@@ -0,0 +1,5 @@
+const contenedor = document.getElementById('elDiv')
+const boton = document.getElementById('boton')
+
+boton.addEventListener('click', ()=> {alert('Hola!')})
+contenedor.addEventListener('click', ()=> {alert('Hola!, soy el div')})
